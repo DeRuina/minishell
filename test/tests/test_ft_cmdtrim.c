3c49ad2114339b19ce1429135fcea7a4af9c21e6
Teemu Spoof <tspoof@student.hive.fi>
2023-04-20 18:35:20 +0300

    added test for ft_cmdtrim

---
+++ test/tests/test_ft_cmdtrim.c
@@ -0,0 +1,19 @@
+#include "unity.h"
+#include "minishell.h"
+
+
+// Probably checks stuff it's not supposed to
+void test_fd_cmdtrim_1337(void)
+{
+	char	*expected[] = {"<Makefile", "cat|", "echo", "$PWD 'hola'", "~/src", "|", "'tr'", "-d", "/", ">outfile"};
+	char	*actual[10] = tokenizer("<Makefile cat| echo \"$PWD 'hola'\" ~/src | 'tr' -d / >outfile");
+
+	TEST_ASSERT_EQUAL_MEMORY(expected, actual, 10);
+}
+
+int test_fd_cmdtrim(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(test_fd_cmdtrim_1337);
+	return UNITY_END();
+}
