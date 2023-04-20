7157979bcbc07f829a5ceb790052655fe3778c8d
Teemu Spoof <tspoof@student.hive.fi>
2023-04-20 18:20:43 +0300

    removed test_split

--- inc/minishell.h
+++ inc/minishell.h
@@ -3,20 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: druina <druina@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 12:17:26 by tspoof            #+#    #+#             */
-/*   Updated: 2023/04/20 15:33:20 by druina           ###   ########.fr       */
+/*   Updated: 2023/04/20 18:20:16 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+# include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 # include "libft.h"
 
 char	*get_token(char **line);
-char **tokenizer(char *line);
+char	**tokenizer(char *line);
 #endif
