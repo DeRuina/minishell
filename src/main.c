8e0d86e923085275c0b067ce5dc84d133df34fd3
Dean Ruina <druina@c2r6p10.hive.fi>
2023-04-20 15:18:08 +0300

    main

--- src/main.c
+++ src/main.c
@@ -6,37 +6,11 @@
 /*   By: druina <druina@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 12:16:50 by tspoof            #+#    #+#             */
-/*   Updated: 2023/04/20 13:18:03 by druina           ###   ########.fr       */
+/*   Updated: 2023/04/20 15:17:47 by druina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-char	*tokenizer(char **line)
-{
-	char	*start;
-	char	*end;
-	char	*whitespace;
-
-	whitespace = ft_strdup(" \t\n\v\f\r");
-	while (ft_strchr(whitespace, *(*line)))
-		(*line)++;
-	if (*(*line) == '\0')
-		return ((*line) = NULL);
-	start = (*line);
-	end = (*line) + ft_strlen((*line));
-	while (!ft_strchr(whitespace, *(*line)))
-		(*line)++;
-	free(whitespace);
-	if ((*line) == end)
-	{
-		(*line = NULL);
-		return (start);
-	}
-	*(*line) = '\0';
-	(*line)++;
-	return (start);
-}
 
 int	main(void)
 {
