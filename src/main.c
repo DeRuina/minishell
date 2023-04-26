08a317b98a88715463653969ba9e36cd164fb1f5
Dean Ruina <druina@c2r5p11.hive.fi>
2023-04-26 09:23:25 +0300

    for merge

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: druina <druina@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 12:16:50 by tspoof            #+#    #+#             */
-/*   Updated: 2023/04/25 10:37:22 by druina           ###   ########.fr       */
+/*   Updated: 2023/04/25 11:35:20 by druina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,17 @@ void	free_2d(char **str)
 
 int	main(int argc, char *argv[])
 {
-	char	*line = "<Makefile cat | echo \"$PWD 'hola'\" ~/src | 'tr' -d / >outfile";
+	char	*line; /*= "<Makefile cat | echo \"$PWD 'hola'\" ~/src | 'tr' -d / >outfile";*/
+	char	*temp;
 	char	**str;
+	line = readline("TERMINAL:");
+	temp = line;
 	(void)argv;
 	argc = 0;
 
 	str = ft_cmd_trim(line);
 	free_2d(str);
+	free(temp);
 
 	return (0);
 }
