c8e0ac747399bbbe512ac2e00a501691f9e17e9b
Teemu Spoof <tspoof@student.hive.fi>
2023-04-28 16:58:36 +0300

    parse_args name change

--- src/ft_parse_args.c
+++ src/ft_parse_args.c
@@ -1,16 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parse_args.c                                       :+:      :+:    :+:   */
+/*   ft_parse_args.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/23 19:03:56 by tspoof            #+#    #+#             */
-/*   Updated: 2023/04/23 19:45:22 by tspoof           ###   ########.fr       */
+/*   Updated: 2023/04/28 16:55:18 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // calls ft_cmd_trim, ft_expand_vars, ft_split_operators and ft_str_trim
-void	parse_args();
+void	ft_parse_args();
