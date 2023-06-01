b3b3979c473154da088b88c6e30a266eca9fed94
Teemu Spoof <tspoof@student.hive.fi>
2023-06-01 15:43:36 +0300

    Fixed $ENV segfault
    ft_var_expand returned NULL if the env variable didn't exist. Now it
    returns the key.

--- src/ft_expand_utils.c
+++ src/ft_expand_utils.c
@@ -6,7 +6,7 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/09 17:26:23 by tspoof            #+#    #+#             */
-/*   Updated: 2023/06/01 15:34:47 by tspoof           ###   ########.fr       */
+/*   Updated: 2023/06/01 15:41:53 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,11 @@ char	*ft_var_expand(t_vec env_vars, char *str)
 	if (*key == '$')
 		value = ft_strdup(key);
 	else
+	{
 		value = ft_getenv(env_vars, key);
+		if (!value)
+			value = key;
+	}
 	free(key);
 	return (value);
 }
