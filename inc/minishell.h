fd10eb6194600d3c66b853856a89c6fee92aa7af
Dean Ruina <ruinadd@gmail.com>
2023-06-03 00:04:57 +0300

    comments added

--- inc/minishell.h
+++ inc/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: druina <druina@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 12:17:26 by tspoof            #+#    #+#             */
-/*   Updated: 2023/06/02 23:48:19 by tspoof           ###   ########.fr       */
+/*   Updated: 2023/06/03 00:03:06 by druina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 # include <stdlib.h> // getenv
 # include <string.h>
 /* #include <sys/syslimits.h>*/
-// # include <linux/limits.h>
+# include <linux/limits.h> // need it for PATH_MAX in linux
 # include <limits.h> // this should work in linux
 # include <sys/wait.h> // for linux
 # include <unistd.h>
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: druina <druina@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 12:16:50 by tspoof            #+#    #+#             */
-/*   Updated: 2023/06/02 23:57:39 by druina           ###   ########.fr       */
+/*   Updated: 2023/06/03 00:04:33 by druina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	minishell(char *line, t_vec *envs)
 	if (is_builtin(head->full_cmd[0]) == 2 && head->next == NULL)
 		return (ft_cd(head->full_cmd, envs));
 	pipe_nbr = piper(line, head);
-	(void)pipe_nbr; // yeah
+	// gotta create a function free_piper
 	ft_executor(head, *envs);
 	free_nodes(head);
 }
