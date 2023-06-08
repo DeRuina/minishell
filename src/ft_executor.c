6b0247f53b205d6657b2e30bd24025107b456fa1
Teemu Spoof <steemum@hotmail.com>
2023-06-08 16:49:35 +0300

    made minor changes to ft_child

--- src/ft_executor.c
+++ src/ft_executor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_executor.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: druina <druina@student.hive.fi>            +#+  +:+       +#+        */
+/*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 17:13:48 by tspoof            #+#    #+#             */
-/*   Updated: 2023/06/06 23:06:13 by druina           ###   ########.fr       */
+/*   Updated: 2023/06/08 16:48:21 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,24 +37,24 @@ int	ft_child(t_node *node, t_vec envv)
 {
 	char	**env;
 
-	env = ft_strenv(envv);
 	if (node->infile == -1)
 		exit(EXIT_FAILURE);
-	else if (node->infile != 0)
+	env = ft_strenv(envv);
+	if (node->infile != 0)
 		if (dup2(node->infile, STDIN_FILENO) < 0)
+			ft_pexit("ft_child: dup2");
+	if (node->outfile != 1)
+		if (dup2(node->outfile, STDOUT_FILENO) < 0)
 			ft_pexit("ft_child: dup2");
 	if (node->infile != 0)
 		close(node->infile);
 	if (node->outfile != 1)
-		if (dup2(node->outfile, STDOUT_FILENO) < 0)
-			ft_pexit("ft_child: dup2");
-	if (node->outfile != 1)
 		close(node->outfile);
-	if (is_builtin(node->full_cmd[0]) != 0)
+	if (is_builtin(node->full_cmd[0]) != NONE)
 		builtin_commands(node->full_cmd, envv);
-	else if (execve(node->full_cmd[0], node->full_cmd, env) < 0)
+	if (execve(node->full_cmd[0], node->full_cmd, env) < 0)
 		ft_pexit(node->full_cmd[0]);
-	return (0);
+	return (1);
 }
 
 static void	ft_close(t_node *node)
