9540405bb818144de61da5d27aa868b4c7634219
Teemu Spoof <steemum@hotmail.com>
2023-06-08 15:51:15 +0300

    added frees to call_builtin exit

--- src/main.c
+++ src/main.c
@@ -6,18 +6,24 @@
 /*   By: tspoof <tspoof@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 12:16:50 by tspoof            #+#    #+#             */
-/*   Updated: 2023/06/07 14:44:25 by tspoof           ###   ########.fr       */
+/*   Updated: 2023/06/08 15:50:47 by tspoof           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include "parser.h"
 
-static int	call_buildin(t_node *head, t_vec *envs)
+static int	call_builtin(t_node *head, t_vec *envs)
 {
 	if (head->full_cmd)
 	{
 		if (is_builtin(head->full_cmd[0]) == EXIT && head->next == NULL)
+		{
+			free_nodes(head);
+			free_envs(*envs);
+			vec_free(envs);
 			exit(EXIT_SUCCESS);
+		}
 		if (is_builtin(head->full_cmd[0]) == CD && head->next == NULL)
 			return (ft_cd(head->full_cmd, envs), 1);
 		if (is_builtin(head->full_cmd[0]) == EXPORT && head->next == NULL)
@@ -42,7 +48,7 @@ static void	minishell(char *line, t_vec *envs)
 	head = ft_parse_args(line, *envs);
 	if (!head)
 		return ;
-	if (call_buildin(head, envs) != 0)
+	if (call_builtin(head, envs) != 0)
 	{
 		free_nodes(head);
 		return ;
@@ -56,26 +62,26 @@ static void	minishell(char *line, t_vec *envs)
 int	main(int argc, char *argv[], char *env[])
 {
 	char	*line;
-	int		wait_times;
-	int		i;
+	// int		wait_times;
+	// int		i;
 	t_vec	envs;
 
+	(void)argv;
 	if (argc != 1)
 		return (ft_putstr_fd("Error: Arguments invalid\n", 0), 1);
-	(void)argv;
 	envs = ft_copyenv(env);
-	i = -1;
+	// i = -1;
 	while (1)
 	{
-		line = readline("TERMINAL:");
+		line = readline("RuiSpo: ");
 		if (strlen(line) > 0)
 			add_history(line);
 		minishell(line, &envs);
-		wait_times = num_of_pipes(line);
+		// wait_times = num_of_pipes(line);
 		free(line);
-		while (++i <= wait_times)
-			wait(NULL);
-		i = -1;
+		// while (++i <= wait_times)
+		// 	wait(NULL);
+		// i = -1;
 	}
 	return (0);
 }
