43787742f8281929f99951269c8976ee6ac31a7a
Teemu Spoof <tspoof@student.hive.fi>
2023-06-19 14:51:12 +0300

    test stuff

--- test/test.c
+++ test/test.c
@@ -11,14 +11,14 @@ int main(int argc, char *argv[], char **envp)
 	(void)argc;
 	(void)argv;
 	(void)envp;
-	test_ft_utils();
-	test_ft_env(envp);
-	test_ft_cmd_trim();
-	test_ft_expand();
+	// test_ft_utils();
+	// test_ft_env(envp);
+	// test_ft_cmd_trim();
+	// // test_ft_expand();
 	// test_ft_split_operators(); // passed arrays in the tests are not allocated so free_2d goes boom.
 	// test_ft_str_trim(); // same thing
-	// // test_ft_parse_args();
+	// // // test_ft_parse_args();
 	// test_ft_get_exec_path(); // ls has different path in linux
-	// // test_ft_executor();
+	// // // test_ft_executor();
 	return (0);
 }
